09125d4357b1c991a6f97ec9d62a232f3c660ecc
nishanthkumarg <nishanthkumarg@google.com>
2016-04-14 12:27:24 -0700

    this is a first commit

---
+++ Application/src/main/java/com/example/android/basicsyncadapter/EntryListActivity.java
@@ -0,0 +1,41 @@
+package com.example.android.basicsyncadapter;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.android.common.logger.Log;
+
+/**
+ * Activity for holding EntryListFragment.
+ */
+public class EntryListActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_list);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");
+
+
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");Log.e("dfasdfasd","asdasdass");
+
+
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");
+
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");
+        Log.e("dfasdfasd","asdasdass");
+
+    }
+}
